a436b562c2a243c31fb981c1c73565cdb01a048c
Cheruiyot Robert <kiruir38@gmail.com>
2024-04-26 16:24:56 +0300

    coomplete lab

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,11 +1,23 @@
-import React from "react";
+import React, {useState} from "react";
 import Item from "./Item";
 
 function ShoppingList({ items }) {
+  const [selectedCategory, setCategory] = useState('All')
+
+  function handleOnChange(event) {
+    setCategory(event.target.value)
+  }
+  const ShoppingListToDisplay = items.filter((item) => {
+    if (selectedCategory === 'All') {
+      return true;
+    }else {
+      return item.category === selectedCategory;
+    }
+  })
   return (
     <div className="ShoppingList">
       <div className="Filter">
-        <select name="filter">
+        <select name="filter" value={selectedCategory} onChange={handleOnChange}>
           <option value="All">Filter by category</option>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
@@ -13,8 +25,10 @@ function ShoppingList({ items }) {
         </select>
       </div>
       <ul className="Items">
-        {items.map((item) => (
-          <Item key={item.id} name={item.name} category={item.category} />
+        {/* {items.map((item) => ( */}
+        {ShoppingListToDisplay.map((item) => (
+          // <Item key={item.id} name={item.name} category={item.category} />
+          <Item key={item.id} name={item.name} category={item.category} isAdded={item.isAdded} />
         ))}
       </ul>
     </div>
